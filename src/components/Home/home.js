026f0aec24b04714aed1d2e518499d777e9ea5cf
aionius <aionius@gmail.com>
2018-08-24 16:11:04 -0400

    added video list

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import NewsSlider from '../widgets/NewsSlider/slider';
 import NewsList from '../widgets/NewsList/newsList';
+import VideoList from '../widgets/VideoList/videoList';
 
 const Home = () => {
     return (
@@ -17,6 +18,12 @@ const Home = () => {
                 type="card"
                 loadmore={true}
                 start={3}
+                amount={3}/>
+            <VideoList
+                type="card"
+                title={true}
+                loadmore={true}
+                start={0}
                 amount={3}/>
         </div>
     );
---
+++ src/components/widgets/VideoList/videoList.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import styles from './videoList.css';
+import { URL } from '../../../config';
+import Button from '../Buttons/buttons';
+import VideosTemplate from '../VideoList/videosList_template';
+
+
+class VideoList extends Component {
+    state = {
+        teams: [],
+        videos: [],
+        start: this.props.start,
+        end: this.props.start + this.props.amount,
+        amount: this.props.amount
+    }
+
+    renderTitle = () => {
+        return this.props.title ? 
+            <h3><strong>NBA</strong> Videos</h3> 
+        : null;
+    }
+
+    renderButton = () => {
+        return this.props.loadmore ? 
+            <Button
+                type="loadmore"
+                loadMore={() => this.loadMore()}
+                cta="Load More Videos">
+            </Button>
+
+        : 
+            <Button
+                type="linkTo"
+                cta="More Videos"
+                linkTo="/videos">
+            </Button>
+    }
+
+    componentWillMount() {
+        this.request(this.state.start, this.state.end);
+    }
+
+    request = (start,end) => {
+        if(this.state.teams.length < 1) {
+            axios.get(`${URL}/teams`)
+                .then(response => {
+                    this.setState({
+                        teams: response.data
+                    })
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+        }
+
+        axios.get(`${URL}/videos?_start=${start}&_end=${end}`)
+            .then(response => {
+                this.setState({
+                    videos: [...this.state.videos, ...response.data],
+                    start: start,
+                    end: end
+                })
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    renderVideos = () => {
+        let template = null;
+        switch(this.props.type) {
+            case('card'):
+                 template = <VideosTemplate data={this.state.videos} teams={this.state.teams} />
+                break;
+            default:
+                template = null;
+                break;
+        }
+
+        return template;
+    }
+
+    loadMore = () => {
+        let end = this.state.end + this.state.amount;
+        this.request(this.state.end, end);
+    }
+
+    render() {
+
+        return(
+            <div className={styles.videoList_wrapper}>
+                {this.renderTitle()}
+                {this.renderVideos()}
+                {this.renderButton()}
+            </div>
+        );
+    }
+}
+
+export default VideoList;
